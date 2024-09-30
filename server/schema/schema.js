725b647d3f8b38cd4c38c703fccd9dc91a81c829
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-09-30 22:07:59 +0530

    Updated Schema.js File for the server

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,16 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList, GraphQLSchema } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLNonNull,
+  GraphQLEnumType
+} from 'graphql';
 import Client from '../models/Client.js';
 import Project from '../models/Project.js';
 
-// import { projects, clients } from '../sample_data.js';
-
-// No Impact on Functionality if you change the name of RootQuery
-// Functionality: The actual functionality of your GraphQL API will not be directly affected.
-// You can still define fields, resolvers, and types as usual. 
-// The queries executed against your API won't require you to reference RootQueryType specifically.
-
+// Project Type Schema
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
   fields: () => ({
@@ -19,13 +21,13 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return Client.findById(parent.clientId);
+        return Client.findById(parent.clientId); // Resolves client by ID
       },
     },
   }),
 });
 
-// Client Type
+// Client Type Schema
 const ClientType = new GraphQLObjectType({
   name: 'Client',
   fields: () => ({
@@ -36,42 +38,106 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Root Query Type
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return Project.find();
+        return Project.find(); // Fetch all projects
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return Project.findById(args.id);
+        return Project.findById(args.id); // Fetch project by ID
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return Client.find();
+        return Client.find(); // Fetch all clients
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return Client.findById(args.id);
+        return Client.findById(args.id); // Fetch client by ID
       },
     },
   },
 });
 
+// Mutation Type
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        });
+        return client.save(); // Save new client
+      },
+    },
+    deleteProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndRemove(args.id); // Delete project by ID
+      },
+    },
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: 'Status',
+            values: {
+              new: { value: 'Not Started' },
+              progress: { value: 'In Progress' },
+              completed: { value: 'Completed' },
+            },
+          }),
+        },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndUpdate(
+          args.id,
+          {
+            name: args.name,
+            description: args.description,
+            status: args.status,
+          },
+          { new: true } // Return updated project
+        );
+      },
+    },
+  },
+});
 
 // Schema Export
 const schema = new GraphQLSchema({
   query: RootQuery,
+
+  // Include the mutation in the schema
+  mutation: mutation,
 });
 
 export default schema;
