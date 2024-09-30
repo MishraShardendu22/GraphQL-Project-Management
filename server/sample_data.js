dae794629e97e41393743b93015f5ef12091c54a
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-09-30 21:46:09 +0530

    Commented Out Use

--- server/sample_data.js
+++ server/sample_data.js
@@ -1,80 +1,79 @@
-// Projects
-const projects = [
-    {
-      id: '1',
-      clientId: '1',
-      name: 'eCommerce Website',
-      description:
-        'This project involves creating an online shopping platform for Indian consumers, enabling them to browse and purchase a wide range of products, including clothing, electronics, and groceries. The platform will support multiple payment options, including UPI, credit cards, and net banking.',
-      status: 'In Progress',
-    },
-    {
-      id: '2',
-      clientId: '2',
-      name: 'Dating App',
-      description:
-        'This dating application aims to connect young professionals in India. Users can create profiles, chat, and plan meetups based on shared interests. The app emphasizes safety and privacy while helping users find meaningful relationships.',
-      status: 'In Progress',
-    },
-    {
-      id: '3',
-      clientId: '3',
-      name: 'SEO Project',
-      description:
-        'The goal of this project is to improve the online visibility of Indian small businesses. We will implement SEO strategies to help them rank higher on search engines, driving more organic traffic to their websites and increasing sales.',
-      status: 'In Progress',
-    },
-    {
-      id: '4',
-      clientId: '4',
-      name: 'Design Prototype',
-      description:
-        'This project involves creating a design prototype for a mobile application focused on fitness and wellness. It will include features like workout tracking, meal planning, and health tips tailored for the Indian audience.',
-      status: 'Done',
-    },
-    {
-      id: '5',
-      clientId: '5',
-      name: 'Auction Website',
-      description:
-        'The auction website will allow users to bid on various items, including antiques, artwork, and electronics. It will feature a user-friendly interface and secure payment gateways to ensure a seamless auction experience.',
-      status: 'In Progress',
-    },
-];
+// // Projects
+// const projects = [
+//     {
+//       id: '1',
+//       clientId: '1',
+//       name: 'eCommerce Website',
+//       description:
+//         'This project involves creating an online shopping platform for Indian consumers, enabling them to browse and purchase a wide range of products, including clothing, electronics, and groceries. The platform will support multiple payment options, including UPI, credit cards, and net banking.',
+//       status: 'In Progress',
+//     },
+//     {
+//       id: '2',
+//       clientId: '2',
+//       name: 'Dating App',
+//       description:
+//         'This dating application aims to connect young professionals in India. Users can create profiles, chat, and plan meetups based on shared interests. The app emphasizes safety and privacy while helping users find meaningful relationships.',
+//       status: 'In Progress',
+//     },
+//     {
+//       id: '3',
+//       clientId: '3',
+//       name: 'SEO Project',
+//       description:
+//         'The goal of this project is to improve the online visibility of Indian small businesses. We will implement SEO strategies to help them rank higher on search engines, driving more organic traffic to their websites and increasing sales.',
+//       status: 'In Progress',
+//     },
+//     {
+//       id: '4',
+//       clientId: '4',
+//       name: 'Design Prototype',
+//       description:
+//         'This project involves creating a design prototype for a mobile application focused on fitness and wellness. It will include features like workout tracking, meal planning, and health tips tailored for the Indian audience.',
+//       status: 'Done',
+//     },
+//     {
+//       id: '5',
+//       clientId: '5',
+//       name: 'Auction Website',
+//       description:
+//         'The auction website will allow users to bid on various items, including antiques, artwork, and electronics. It will feature a user-friendly interface and secure payment gateways to ensure a seamless auction experience.',
+//       status: 'In Progress',
+//     },
+// ];
 
-// Clients
-const clients = [
-    {
-      id: '1',
-      name: 'Rohit Sharma',
-      email: 'rohit.sharma@gmail.com',
-      phone: '98765-43210',
-    },
-    {
-      id: '2',
-      name: 'Sneha Rani',
-      email: 'sneha.rani@gmail.com',
-      phone: '91234-56789',
-    },
-    {
-      id: '3',
-      name: 'Aaryan Kapoor',
-      email: 'aaryan.kapoor@gmail.com',
-      phone: '82345-67890',
-    },
-    {
-      id: '4',
-      name: 'Sakshi Verma',
-      email: 'sakshi.verma@gmail.com',
-      phone: '76543-21098',
-    },
-    {
-      id: '5',
-      name: 'Viraj Patel',
-      email: 'viraj.patel@gmail.com',
-      phone: '67890-12345',
-    },
-];
+// // Clients
+// const clients = [
+//     {
+//       id: '1',
+//       name: 'Rohit Sharma',
+//       email: 'rohit.sharma@gmail.com',
+//       phone: '98765-43210',
+//     },
+//     {
+//       id: '2',
+//       name: 'Sneha Rani',
+//       email: 'sneha.rani@gmail.com',
+//       phone: '91234-56789',
+//     },
+//     {
+//       id: '3',
+//       name: 'Aaryan Kapoor',
+//       email: 'aaryan.kapoor@gmail.com',
+//       phone: '82345-67890',
+//     },
+//     {
+//       id: '4',
+//       name: 'Sakshi Verma',
+//       email: 'sakshi.verma@gmail.com',
+//       phone: '76543-21098',
+//     },
+//     {
+//       id: '5',
+//       name: 'Viraj Patel',
+//       email: 'viraj.patel@gmail.com',
+//       phone: '67890-12345',
+//     },
+// ];
 
-export { projects, clients };
-    
+// export { projects, clients };
