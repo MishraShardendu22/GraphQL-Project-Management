58a1a7b6e697ac1b2adf017ce2049ac0a2875384
Shardendu Mishra <shardendumishra01@gmail.com>
2024-10-01 12:30:01 +0530

    Setting up CORS and Connecting Front and Back End

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,29 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import {Footer} from './components/Footer';
+import { ApolloProvider,ApolloClient,InMemoryCache } from '@apollo/client';
+
+// InMemoryCache in Apollo Client stores GraphQL data in memory, allowing quick access to previously fetched results without additional network requests. 
+// It normalizes data, preventing duplication and enabling efficient updates when mutations occur. By automatically managing the cache, it enhances performance, keeps the UI consistent with the server's state, and provides developers with flexibility to read and write data directly.
+// Overall, it plays a crucial role in optimizing data management in React applications using GraphQL.
+
+
+// Client sets up the Apollo Client to connect to the GraphQL server.
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache : new InMemoryCache()
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+      <ApolloProvider client={client}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </ApolloProvider>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import { graphqlHTTP } from 'express-graphql';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import cors from 'cors'; // Import CORS
 
 import connectDB from './config/connectDB.js';
 import schema from './schema/schema.js';
@@ -13,27 +14,20 @@ const port = process.env.PORT || 3000;
 
 connectDB();
 
-app.use("/graphql",graphqlHTTP (
-    {
-        schema,
-        graphiql: process.env.NODE_ENV === 'development'
-    }
-))
+// Enable CORS
+app.use(cors());
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}));
 
 app.listen(port, () => {
-    // For Testing Purpose , env file wasn connecting,
-    // moved it a layer up it started working 
-
-    // console.log(process.env.NODE_ENV);
-    // console.log(process.env.PORT);
-
     console.log(`\nServer is running on port ${port}`.blue.bold.underline);
     console.log(`Click To Connect to http://localhost:${port}`.blue.bold.underline);
-})
-    
+});
 
 // Trial Server Working 
-    
 // app.get('/', (req, res) => {
 //     res.send('Hello World');
-// })
+// });
